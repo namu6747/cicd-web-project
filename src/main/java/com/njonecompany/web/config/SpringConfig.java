9c10913bde35526ccbd0c27010e474f3d416d7f5
namu6747 <namu6747@naver.com>
2023-07-30 00:35:19 +0900

    Import package for filter

--- src/main/java/com/njonecompany/web/config/SpringConfig.java
+++ src/main/java/com/njonecompany/web/config/SpringConfig.java
@@ -3,11 +3,14 @@ package com.njonecompany.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.servlet.Filter;
 
 @EnableWebMvc
 @Configuration
